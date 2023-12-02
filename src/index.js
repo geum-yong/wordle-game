e5ac6edc6bab5480cb75a16276ec80a19598a948
Ben <geumyong@carrotins.com>
2023-12-02 20:30:18 +0900

    feat: app container layout 구현

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import onRenderHeader from "./components/Header/index.js";
+import onRenderAppContainer from "./components/AppContainer/index.js";
 
 const app = document.getElementById("app");
 
 const onCreateComponent = ({ parentElement, childElement, event }) => {
   parentElement.appendChild(childElement);
   event && event();
-
-  return childElement;
 };
 
 window.addEventListener("DOMContentLoaded", () => {
+  const { element: Appcontainer } = onRenderAppContainer();
+  onCreateComponent({ parentElement: app, childElement: Appcontainer });
+
   const { element: Header } = onRenderHeader("Wordle");
-  onCreateComponent({ parentElement: app, childElement: Header });
+  onCreateComponent({ parentElement: Appcontainer, childElement: Header });
 });
