1ff8f06de578f8dbe8d326404098fbd3e4e366c1
Ben <geumyong@carrotins.com>
2023-12-03 13:38:02 +0900

    feat: toast popup layout 및 event 구현

---
+++ src/components/TastPopup/component.js
@@ -0,0 +1,9 @@
+const ToastPopup = () => {
+  const toastPopup = document.createElement("div");
+  toastPopup.id = "toastPopup";
+  toastPopup.classList.add("toastPopup");
+
+  return toastPopup;
+};
+
+export default ToastPopup;
